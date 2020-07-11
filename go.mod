10a2a43357cf5afebef735c738807c67cb5e9b36
k.log <headofhell@gmail.com>
2020-07-11 11:19:59 +0400

    Add access and refresh token generation

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module app
 
 go 1.14
 
-require go.mongodb.org/mongo-driver v1.3.4
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	go.mongodb.org/mongo-driver v1.3.4
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+)
--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/dgrijalva/jwt-go"
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
 )
+
+type AccessTokenClaims struct {
+	jwt.StandardClaims
+	GUID string
+}
 
 func main() {
 
@@ -24,8 +33,39 @@ func main() {
 		}
 	}()
 
+	secret := []byte("some secret string")
+
 	http.HandleFunc("/1", func(w http.ResponseWriter, r *http.Request) {
+		guidParam := r.URL.Query()["guid"]
+
+		if len(guidParam) != 1 {
+			panic("invalid guid")
+		}
+
+		guid := guidParam[0]
+
+		rfTkn, err := uuid.New()
+
+		if err != nil {
+			panic(err)
+		}
+
+		RefreshToken := rfTkn[:]
+
+		AccessToken, err := generateAccessToken(AccessTokenClaims{
+			GUID: guid,
+			StandardClaims: jwt.StandardClaims{
+				ExpiresAt: time.Now().Add(time.Minute * 30).Unix(),
+			},
+		}).SignedString(secret)
+
+		if err != nil {
+			panic(err)
+		}
+
 		fmt.Println("/1")
+		fmt.Println("AccessToken", AccessToken)
+		fmt.Println("RefreshToken", base64.StdEncoding.EncodeToString(RefreshToken))
 	})
 
 	http.HandleFunc("/2", func(w http.ResponseWriter, r *http.Request) {
@@ -48,4 +88,12 @@ func connectToDatabase(cntnStr string) (*mongo.Client, error) {
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(cntnStr))
 	return client, err
+}
+
+func generateRefreshToken() (uuid.UUID, error) {
+	return uuid.New()
+}
+
+func generateAccessToken(claims AccessTokenClaims) *jwt.Token {
+	return jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
 }
